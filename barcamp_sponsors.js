b7cbef10104d7b1d3b25599f1ba7c7990d6a6ca0
Sumeet Jain <sumeet@sumeetjain.com>
2017-07-25 08:55:35 -0500

    Clean up JS a bit

--- barcamp_sponsors.js
+++ barcamp_sponsors.js
@@ -25,15 +25,9 @@ function sponsorData(sponsorGroup){
 
 sponsors = sponsorGroups.map(function(g) { return sponsorData(g) });
 
-// ejs.renderFile("index.ejs", sponsors, {}, function(err, str){
-//     console.log(str);
-//     // str => Rendered HTML string
-// });
-
 ejs.renderFile("index.ejs", sponsors, (err, result) => {
     if (err) {
         logger.log('info', 'error encountered: ' + err);
-        // throw err;
     }
     else {
         try {
@@ -46,36 +40,3 @@ ejs.renderFile("index.ejs", sponsors, (err, result) => {
 
     }
 });
-
-// [ { path: 'sponsors/1-platinum',
-//     full: 'sponsors/1-platinum/Agape_White.png',
-//     file: 'Agape_White.png',
-//     name: 'Agape_White' },
-//   { path: 'sponsors/1-platinum',
-//     full: 'sponsors/1-platinum/SCO_White.png',
-//     file: 'SCO_White.png',
-//     name: 'SCO_White' },
-//   { path: 'sponsors/2-gold',
-//     full: 'sponsors/2-gold/Alamo_White.png',
-//     file: 'Alamo_White.png',
-//     name: 'Alamo_White' },
-//   { path: 'sponsors/2-gold',
-//     full: 'sponsors/2-gold/LiquidSunshine_White.png',
-//     file: 'LiquidSunshine_White.png',
-//     name: 'LiquidSunshine_White' },
-//   { path: 'sponsors/3-silver',
-//     full: 'sponsors/3-silver/Hayneedle_White.png',
-//     file: 'Hayneedle_White.png',
-//     name: 'Hayneedle_White' },
-//   { path: 'sponsors/4-bronze',
-//     full: 'sponsors/4-bronze/Dylan_White.png',
-//     file: 'Dylan_White.png',
-//     name: 'Dylan_White' },
-//   { path: 'sponsors/4-bronze',
-//     full: 'sponsors/4-bronze/ESS_White.png',
-//     file: 'ESS_White.png',
-//     name: 'ESS_White' },
-//   { path: 'sponsors/4-bronze',
-//     full: 'sponsors/4-bronze/ScreenInk_White.png',
-//     file: 'ScreenInk_White.png',
-//     name: 'ScreenInk_White' } ]
